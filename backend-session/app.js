b3591458cbd256bef695fe869b506809fc956f0c
tomydev <tramosyahari@gmail.com>
2024-09-05 15:38:14 -0300

    se hicieron algunos cambios

--- backend-session/app.js
+++ backend-session/app.js
@@ -31,27 +31,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
 
 
-// Middlewares 
-/*
-app.use(cors({ // Permitir solicitudes desde el front-end
-    origin: [
-        'http://localhost:5500',
-        'http://localhost:3000',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // Habilitar envío de cookies
-}));
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-    secret: 'mi_secreto',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { 
-        secure: false, // true solo si usas HTTPS
-        httpOnly: true, // evita acceso a cookie desde JavaScript del cliente
-        // sameSite: 'lax' // permite envío de cookies en navegadores modernos
-    }
-}));
-*/
--- backend-session/controllers/auth.controllers.js
+++ backend-session/controllers/auth.controllers.js
@@ -37,24 +37,6 @@ export const login = async (req, res) => {
 };
 
 
-/*app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    // Buscar usuario
-    const user = database.user.find(u => u.username === username && u.password === password);
-
-    if (user) {
-        // Guardar información del usuario en la sesión
-        req.session.userId = user.id;
-        req.session.username = user.username;
-
-        return res.json({ 
-            message: 'Inicio de sesión exitoso', 
-            user: { id: user.id, username: user.username } });
-    } else {
-        return res.status(401).json({ message: 'Credenciales incorrectas' });
-    }
-});*/
-
 //register
 export const register = async (req, res) => {
     const { username, password } = req.body;
@@ -100,16 +82,7 @@ export const logout = async (req, res) => {
         return res.json({ message: 'Sesión cerrada exitosamente' });
     });
 }
-/*app.post('/logout', (req, res) => {
-    console.log(req.session)
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).json({ message: 'Error al cerrar la sesión' });
-        }
-        res.clearCookie('connect.sid'); // Nombre de cookie por defecto para express-session
-        return res.json({ message: 'Sesión cerrada exitosamente' });
-    });
-});*/
+
 
 
 
@@ -121,13 +94,3 @@ export const session = async (req, res) => {
         res.status(401).json({ message: 'No hay sesión activa' });
     }
 };
-
-/*app.get('/session', (req, res) => {
-    if (req.session.userId) {
-        return res.json({ 
-            loggedIn: true, 
-            user: { id: req.session.userId, username: req.session.username } });
-    } else {
-        return res.status(401).json({ loggedIn: false, message: 'No hay sesión activa' });
-    }
-});*/
